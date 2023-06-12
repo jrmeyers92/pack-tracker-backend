4593a2e761b5360a635e5636c837bb54fddfdb6c
Jake Meyers <jakermeyers92@gmail.com>
2023-06-12 17:46:46 -0500

    change sub document type of items in catagory model

--- models/catagoryModel.js
+++ models/catagoryModel.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose');
+const Item = require('./itemModel');
+
+const itemSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  weight: Number,
+  unitOfMeasure: {
+    type: String,
+    enum: ['oz', 'lb', 'kg', 'g']
+  }
+});
 
 const catagorySchema = new mongoose.Schema({
-  name: String,
-  items: [
-    {
-      type: mongoose.Schema.ObjectId,
-      ref: 'item'
-    }
-  ]
+  catagory_name: String,
+  items: [itemSchema]
 });
+
+// catagorySchema.pre(/^find/, function(next) {
+//   this.populate({
+//     path: 'items',
+//     select: '-__v'
+//   });
+//   next();
+// });
 
 const Catagory = mongoose.model('Catagory', catagorySchema);
 
